9712ad55a0f794dfad5026af874fe1d44b087a97
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-12-07 14:50:20 +0100

    hedde ditt nästa val

--- julproject.py
+++ julproject.py
@@ -8,11 +8,13 @@ while variable <4>0:
     print('after having a dream about santa getting kidnapped you knew you had to save him but before going to save santa you had to get more information')
     text=int(input('you can choose between the following 1.the wise tree 2. an information broker'))
     if text == 1 
-        print 
+        print('you went to the wise tree to see if he knew where santa was')
+        print('the tree said it knew and then told you santa was held in a warehouse two towns over')
+        val1=int(input('now that you had the information'))
 
     elif text == 2
         print('you went to a information broker because surely they had the information you wanted so you went and asked where santa was being kept by the kidnappers')
-        val2=int(input('he said sure but it will cost you will you pay the fee for the information 1. yes i will pay the 1000$ or also known all my money 2. no'))
+        val2=int(input("he said sure but it will cost you will you pay the fee for the information 1. yes i will pay the 1000$ or also known all my money 2. no you won't pay and goes back home to choose again "))
             if val2 == 1
                 print("congrats you're now broke but atleast you got the information or so you thought ")
                 print('because what the information brokey says next makes you think you have wasted that money')
