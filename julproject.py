fff932a22fa46cf57dcda20e0ac92513894b1029
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-12-07 13:20:20 +0100

    the beginning
    texten man får innan man får börja skriva sina val

--- julproject.py
+++ julproject.py
@@ -1,1 +1,3 @@
-print('')
+print('christmas is in danger! Santa claus is kidnapped!')
+print("you're not really anyone special but you decided it was you that was going to save christmas.")
+print('when playing this game you get 3 choices each time and have to decide between them by writing either 1, 2 or 3 no other options are available')
