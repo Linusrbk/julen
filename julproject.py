36be5d9ac72bf787c814e0bed2642e2df1c709cc
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-12-07 14:14:45 +0100

    börjat skriva alternativ
    vet inte hur bra detta kommer gå

--- julproject.py
+++ julproject.py
@@ -1,3 +1,13 @@
-print('christmas is in danger! Santa claus is kidnapped!')
+print('christmas is in danger! Santa is kidnapped!')
 print("you're not really anyone special but you decided it was you that was going to save christmas.")
-print('when playing this game you get 3 choices each time and have to decide between them by writing either 1, 2 or 3 no other options are available')
+print('when playing this game you get up to 3 choices each time and have to decide between them by writing either 1, 2 or 3 no other options are available and only one option is correct')
+print('.......................')
+variable = 1
+text = 0
+while variable <4>0:
+    print('after having a dream about santa getting kidnapped you knew you had to save him but before going to save santa you had to get more information')
+    text=int(input('you can choose between the following 1.the wise tree 2. an information broker'))
+    if text == 1 
+        print 
+    
+    
